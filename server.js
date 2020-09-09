8ba7b95fec8a3bd7259b557bc77cd25174faa623
unknown <rohit.raj.bhandari07@gmail.com>
2020-09-09 22:56:47 +0300

    displaying the content in index.ejs

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
-
+const articleRouter = require("./routes/articles");
 const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //set Index.ejs as a Html
+
+app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [
+    {
+      title: "test articles",
+      createdAt: new Date().now,
+      description: "test descriptions",
+    },
+  ];
+  res.render("index", { articles: articles });
 });
 app.listen(5000);
